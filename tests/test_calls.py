869fe0b0efe7854f31d520cf962057546ca026ab
Nancy Hansen <nhansen@mail.nih.gov>
2026-01-12 11:25:24 -0500

    Fixes to the test suite

--- tests/test_calls.py
+++ tests/test_calls.py
@@ -10,23 +10,23 @@ from GQC import alignparse
 from GQC import mummermethods
 from GQC import bedtoolslib
 
-def test_configs():
-    args = bench.parse_arguments(['-c', 'tests/testconfig.txt', '-b', 'blah', '-r', 'blah', '-q', 'blah', '-p', 'blah'])
-    configvals = bench.read_config_data(args)
-    assert configvals['resourcedir'] == 'resources'
+#def test_configs():
+    #args = bench.parse_arguments(['-c', 'GQC/benchconfig.txt', '-b', 'blah', '-r', 'blah', '-q', 'blah', '-p', 'blah'])
+    #configvals = bench.read_config_data(args)
+    #assert configvals['resourcedir'] == 'resources'
 
 def test_checkforprogs():
     bench.check_for_bedtools()
     no_rscript = bench.check_for_R()
 
 def test_createoutputdir():
-    args = bench.parse_arguments(['-c', 'tests/testconfig.txt', '-b', 'tests/test.sort.bam', '-r', 'tests/testbenchmark.fasta.gz', '-q', 'tests/testassembly.fasta.gz', '-p', 'tests/testrun'])
+    args = bench.parse_arguments(['-c', 'GQC/benchconfig.txt', '-b', 'tests/test.sort.bam', '-r', 'tests/testbenchmark.fasta.gz', '-q', 'tests/testassembly.fasta.gz', '-p', 'tests/testrun'])
     configvals = bench.read_config_data(args)
     outputdir = output.create_output_directory(args.prefix)
     assert os.path.isdir(outputdir)
 
 def test_writebedfiles():
-    args = bench.parse_arguments(['-c', 'tests/testconfig.txt', '-b', 'tests/test.sort.bam', '-r', 'tests/testbenchmark.fasta.gz', '-q', 'tests/testassembly.fasta.gz', '-p', 'tests/testrun'])
+    args = bench.parse_arguments(['-c', 'GQC/benchconfig.txt', '-b', 'tests/test.sort.bam', '-r', 'tests/testbenchmark.fasta.gz', '-q', 'tests/testassembly.fasta.gz', '-p', 'tests/testrun'])
     configvals = bench.read_config_data(args)
 
     refobj = pysam.FastaFile(args.reffasta)
